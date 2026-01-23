8b486688879819a6f0809f02c10fea7a77bb8c3c
shreesha-21 <imsmb21@gmail.com>
2026-01-23 23:59:23 +0530

    Added an Event listener to handle user leaving the session abruptly

---
+++ src/main/java/com/example/chatApp/Config/WebSockerEventListner.java
@@ -0,0 +1,41 @@
+package com.example.chatApp.Config;
+
+import com.example.chatApp.model.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
+
+//  Event listener to handle user leaving abruptly
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSockerEventListner {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
+
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+
+            ChatMessage chatMessage = ChatMessage
+                    .builder()
+                    .type(ChatMessage.MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+
+    }
+
+}
