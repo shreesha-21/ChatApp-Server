c9511c78d6f68b21d2afc6fe900a775aa49ee783
shreesha-21 <imsmb21@gmail.com>
2026-01-23 19:22:33 +0530

    Initialized a new Springboot project

---
+++ src/main/java/com/example/chatApp/Config/WebSockerConfig.java
@@ -0,0 +1,32 @@
+package com.example.chatApp.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
+
+//  Configuration file for Websocket
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSockerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*");
+//                .withSockJS(); // Remove this for native Android apps, keep if using a Web Browser fallback
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        //  Path to which server broadcasts messages
+        registry.enableSimpleBroker("/topic");
+
+        // Path to which the client sends the messages to the server
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+}
