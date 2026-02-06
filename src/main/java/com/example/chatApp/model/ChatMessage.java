824c683854e9204b61456ff6c28981ebf173ed84
shreesha-21 <imsmb21@gmail.com>
2026-02-06 19:43:30 +0530

    Made some preparations to add one v one chat and a db

--- src/main/java/com/example/chatApp/model/ChatMessage.java
+++ src/main/java/com/example/chatApp/model/ChatMessage.java
@@ -1,17 +1,32 @@
 package com.example.chatApp.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 //  Model for the message in chat
+@Entity // Makes this a db table
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String content;
+    private String recipient;   // For one on one chats
     private String sender;
+
+    @Enumerated(EnumType.STRING)
     private MessageType type;
+
+    private String roomId; // Null for a private message
+    private Date timeStamp;
 
     public enum MessageType {
         CHAT,
