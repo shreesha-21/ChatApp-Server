0be3521111d4ee41269d0a98618dfd6d8c26138e
shreesha-21 <imsmb21@gmail.com>
2026-02-08 15:08:31 +0530

    Added a service layer to the app

---
+++ src/main/java/com/example/chatApp/service/ChatService.java
@@ -0,0 +1,35 @@
+package com.example.chatApp.service;
+
+import com.example.chatApp.model.ChatMessage;
+import com.example.chatApp.repository.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// Service which handles chat services
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        String chatId = getChatId(chatMessage.getSender(), chatMessage.getRecipient(), chatMessage.isGroup());
+        chatMessage.setChatId(chatId);
+        return chatMessage;
+    }
+
+    // generates unique chat id for the chats
+    private String getChatId(String sender, String recipient, boolean isGroup) {
+        if (isGroup) {
+           return recipient + "_group";
+        }
+        else {
+            if (sender.compareTo(recipient) < 0) {
+                return sender + "_" + recipient;
+            } else {
+                return recipient + "_" + sender;
+            }
+        }
+    }
+
+}
